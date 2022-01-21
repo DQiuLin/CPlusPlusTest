2a09b9e0f7d7972c43aa06ca0a9ed0ca47fc983d
DQiuLin <duanqiulin_1998@163.com>
2022-01-21 08:52:04 +0800

    1

--- include/common.h
+++ include/common.h
@@ -9,18 +9,36 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <random>
 #include <cstring>
 #include <string>
+#include <array>
 #include <vector>
+#include <list>
 #include <set>
 #include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::array;
 using std::vector;
+using std::list;
 using std::set;
 using std::map;
+using std::deque;
+using std::queue;
+using std::stack;
+using std::unordered_set;
+using std::unordered_map;
 
 #endif //CPLUSPLUSTEST_COMMON_H
